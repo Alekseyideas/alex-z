32fc20dd0cb522b46c12ccc15232b07ef1727636
Alekseyideas <zelenskiy.a@udtech.co>
2020-06-02 17:37:11 +0300

    init

---
+++ src/components/slide.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components';
+
+export const Slide = () => {
+	return (
+		<SlideWrapperS>
+
+		</SlideWrapperS>
+	)
+}
+
+
+const SlideWrapperS = styled.section`
+	height: 100vh;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+`;
+

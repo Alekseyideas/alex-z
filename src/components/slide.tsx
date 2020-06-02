39ffd0fda7bd8223f18d8e19eaabb76f3aaccbab
Alekseyideas <zelenskiy.a@udtech.co>
2020-06-02 18:56:18 +0300

    added airnbn config

--- src/components/slide.tsx
+++ src/components/slide.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import * as React from 'react';
 import styled from 'styled-components';
+import { Container } from 'react-bootstrap';
 
 export const Slide = () => {
 	return (
 		<SlideWrapperS>
-
+			<Container>
+				<SlideTitleS>Hi, I'm Aleksey Zelenskiy</SlideTitleS>
+				<SlideSubtitleS>I`m Full Stack Developer</SlideSubtitleS>
+			</Container>
 		</SlideWrapperS>
-	)
-}
-
+	);
+};
 
 const SlideWrapperS = styled.section`
 	height: 100vh;
@@ -16,5 +19,16 @@ const SlideWrapperS = styled.section`
 	justify-content: center;
 	align-items: center;
 	flex-direction: column;
+	background: black;
+	font-size: 50px;
 `;
 
+const SlideTitleS = styled.h1`
+	color: white;
+	font-size: 1em;
+`;
+const SlideSubtitleS = styled.h5`
+	font-weight: 300;
+	color: white;
+	font-size: 0.5em;
+`;
